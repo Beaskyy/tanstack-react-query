6ffd975e2e2372fd33e5e78b8d8fb6b2bdb2395d
Beaskyy <afeez.akintobi@yahoo.com>
2025-02-24 12:18:50 +0100

    use mutations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Beasky from "./components/beasky";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider
-      className="App"
-      client={queryClient}
-    >
+    <div>
       <Beasky />
-    </QueryClientProvider>
+    </div>
   );
 }
 
--- src/components/beasky.jsx
+++ src/components/beasky.jsx
@@ -1,35 +1,55 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-const fetchData = () => {
-  return fetch("https://jsonplaceholder.typicowede.com/users")
-  .then((res) => res.json())
-  .catch((error) => {
-    throw new Error("Failed to fetch data: " + error);
-  })
-}
-
-console.log('beasky')
+const fetchData = async () => {
+  return await fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json()
+  );
+};
 
 const Beasky = () => {
-  const {data, isLoading, isError, error} = useQuery({
+  const { data, isLoading, error } = useQuery({
     queryKey: ["users"],
     queryFn: fetchData,
-  })
+  });
 
-  if(isLoading) <h2>Loading</h2>
-  if(isError) <h2>{error.message}</h2>
 
-  console.log(data)
+const { mutate, isPending, isError, isSuccess } = useMutation({
+  mutationFn: async (newPost) =>
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    }),
+});
+
+  if (isLoading) <h2>Loading...</h2>;
+  if (isError) <h2>{error.message}</h2>;
+
+  console.log(data);
   return (
     <div>
-      {data?.map(item => (
+      <button
+        onClick={() => {
+          mutate({
+            id: 2034,
+            title: "New Post",
+            body: "This is a new post",
+            userId: 1000,
+          });
+        }}
+      >
+        {isPending ? "Adding..." : "Add user"}
+      </button>
+      {data?.map((item) => (
         <div key={item.id}>
-          <h3>{item.name}</h3>
-          <p>{item.email}</p>
+          <h3>{item.title}</h3>
+          <p>{item.body}</p>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Beasky
+export default Beasky;
